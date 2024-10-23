96c5a067ed2145b4a7116cfa7091e30e1d00820d
istamosh <63596969+istamosh@users.noreply.github.com>
2024-10-23 21:49:33 +0700

    added clerk, nav elements

---
+++ src/app/(marketing)/_components/NavBar.tsx
@@ -0,0 +1,34 @@
+import BrandLogo from "@/components/BrandLogo";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+  const navElements = [
+    { href: "/", classname: "mr-auto", content: <BrandLogo /> },
+    { href: "#", classname: "text-lg", content: "Features" },
+    { href: "/#pricing", classname: "text-lg", content: "Pricing" },
+    { href: "#", classname: "text-lg", content: "About" },
+  ];
+  return (
+    <header className="flex py-6 shadow-xl fixed top-0 w-full z-10 bg-backround/95">
+      <nav className="flex items-center gap-10 container font-semibold">
+        {navElements.map((el, i) => (
+          <Link href={el.href} key={i} className={el.classname}>
+            {el.content}
+          </Link>
+        ))}
+        <span className="text-lg">
+          <SignedIn>
+            <Link href="/dashboard">Dashboard</Link>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton>Login</SignInButton>
+          </SignedOut>
+        </span>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
